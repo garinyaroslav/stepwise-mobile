b5fcb251502e540d67ce395f08f0e3c338c9da46
garinyaroslav <garinyaroslav13@mail.ru>
2025-10-25 20:50:30 +0300

    changed login fragment and created new primary logo

--- app/src/main/java/com/github/stepwise/StudentActivity.kt
+++ app/src/main/java/com/github/stepwise/StudentActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.github.stepwise.databinding.ActivityStudentBinding
@@ -12,16 +13,19 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class StudentActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityStudentBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityStudentBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbarStudent)
+
         val navView: BottomNavigationView = binding.navViewStudent
 
         val navController = findNavController(R.id.nav_host_fragment_student)
-        val appBarConfiguration = AppBarConfiguration(
+        appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.student_home_fragment,
                 R.id.student_projects_fragment,
@@ -30,5 +34,11 @@ class StudentActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    // Обработка Up (стрелки назад в тулбаре)
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_student)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
